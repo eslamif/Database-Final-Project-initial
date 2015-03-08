c540d34e77e50d4c312c236f7e72fd18f10a92f1
Frank Eslami <eslamif@oregonstate.edu>
2015-03-08 03:12:25 -0700

    phpFunctions.php - validate access via post request only

--- src/phpFunctions.php
+++ src/phpFunctions.php
@@ -4,6 +4,14 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-Type: text/html');
 
+//Validate Access via POST REQUEST ONLY
+$method = $_SERVER['REQUEST_METHOD'];
+if(strtolower($method) != 'post') {
+	echo "You may not access this page directly. Please go back to the 
+	<a href=http://localhost/myhost-exemple/Final%20Project/src/>Login</a> page";
+}
+
+
 //Register New User
 if(isset($_GET['action']) && $_GET['action'] == 'register') {
 	$f_name = $_POST['f_name'];
