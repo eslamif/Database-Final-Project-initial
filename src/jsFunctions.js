cfbfe92bbd6de8632fac5a8509fb26c16647a17f
Frank Eslami <eslamif@oregonstate.edu>
2015-03-08 00:03:41 -0800

    Register form variables passed to php successfully

--- src/jsFunctions.js
+++ src/jsFunctions.js
@@ -3,7 +3,13 @@ $(document).ready(
 //Register user
 function() {
 	$('#register').click(function() {
-		$.post("phpFunctions.php", {}, 
+		$.post("phpFunctions.php?action=register", 
+		{
+			f_name: $('#f_name').val(), 
+			l_name: $('#l_name').val(),
+			email: $('#email').val(),
+			pass: $('#pass').val()
+		}, 
 			function(regResponse) {
 				$('#regConfirmation').text(regResponse);
 			});
--- src/phpFunctions.php
+++ src/phpFunctions.php
@@ -4,5 +4,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-Type: text/html');
 
-	echo "Callback worked.";
+//Register New User
+if(isset($_GET['action']) && $_GET['action'] == 'register') {
+	$f_name = $_POST['f_name'];
+	$l_name = $_POST['l_name'];
+	$email = $_POST['email'];
+	$pass = $_POST['pass'];	
+}
+
 ?>
